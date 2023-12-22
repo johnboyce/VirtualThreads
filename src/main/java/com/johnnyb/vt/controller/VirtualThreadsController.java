0fb4a3420d64aac71c4d146ca01062ed46693f4b
johnboyce <johnboyce@comcast.net>
2023-12-21 23:41:20 -0500

    Configure for port 3000

--- src/main/java/com/johnnyb/vt/controller/VirtualThreadsController.java
+++ src/main/java/com/johnnyb/vt/controller/VirtualThreadsController.java
@@ -1,17 +1,20 @@
 package com.johnnyb.vt.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("virtual")
 public class VirtualThreadsController {
     public VirtualThreadsController() {
     }
 
+    @GetMapping
+    public String root() {
+        return "JOHNNY B Virtual Threads";
+    }
+
     @GetMapping("hi")
-    public String hi (){
+    public String hi() {
         return "Virtual Threads";
     }
 }
