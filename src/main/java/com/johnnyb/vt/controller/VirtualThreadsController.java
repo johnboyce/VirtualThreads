a4dddfb7cdc1b79c7e8f5f62cb9213b5acc88015
johnboyce <johnboyce@comcast.net>
2023-12-22 10:51:31 -0500

    Add a httpbin service

--- src/main/java/com/johnnyb/vt/controller/VirtualThreadsController.java
+++ src/main/java/com/johnnyb/vt/controller/VirtualThreadsController.java
@@ -1,11 +1,21 @@
 package com.johnnyb.vt.controller;
 
+import com.johnnyb.vt.service.HttpBinService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class VirtualThreadsController {
-    public VirtualThreadsController() {
+    private static final Logger log = LoggerFactory.getLogger(VirtualThreadsController.class);
+    private final HttpBinService httpBinService;
+
+    public VirtualThreadsController(HttpBinService httpBinService) {
+        this.httpBinService = httpBinService;
     }
 
     @GetMapping
@@ -13,8 +23,12 @@ public class VirtualThreadsController {
         return "JOHNNY B Virtual Threads";
     }
 
-    @GetMapping("hi")
-    public String hi() {
-        return "Virtual Threads";
+    @GetMapping("delay/{seconds}")
+    public String delay(@PathVariable int seconds) {
+        ResponseEntity<Void> delay = httpBinService.delay(seconds);
+        log.info("{} on {}", delay.getStatusCode(), Thread.currentThread());
+        return Thread.currentThread().toString();
     }
+
+
 }
